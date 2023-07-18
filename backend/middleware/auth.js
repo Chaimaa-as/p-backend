fe57fc6cadb8dddf745036a4d62b62cf6fcb48a4
ChaimaaAsri <108627246+Chaimaa-as@users.noreply.github.com>
2023-07-18 21:07:03 +0200

    test-backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,57 @@
-// Configure Auth Key
 const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, process.env.TOKEN);
-    const userId = decodedToken.userId;
-    req.auth = {
-      userId: userId,
-    };
-    next();
-  } catch (error) {
-    res.status(401).json({ error });
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //Get token from header
+      token = req.headers.authorization.split(" ")[1]; // token, the variable we set above
+
+      // verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get the user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
   }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+module.exports = {
+  protect,
 };
+// // Configure Auth Key
+// const jwt = require("jsonwebtoken");
+
+// module.exports = (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization.split(" ")[1];
+//     const decodedToken = jwt.verify(token, process.env.TOKEN);
+//     const userId = decodedToken.userId;
+//     req.auth = {
+//       userId: userId,
+//     };
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ error });
+//   }
+// };
 
 // Ce code est un middleware d'authentification basé sur JSON Web Tokens (JWT)
 // pour une application Node.js avec Express. Il sert à vérifier si l'utilisateur
